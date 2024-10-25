a267e126e4af8845894276b3cd81ed98846dab12
manish8271 <manish45q@gmail.com>
2024-10-25 09:53:17 +0530

    first commit of file

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,64 @@
+import React, {useId} from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",   
+    onClearAmount,    
+}) {
+   const amountInputId = useId()
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    />
+                
+            </div>
+                    {!amountDisable && (
+                            <button
+                                type="button"
+                                className="absolute right-36 top-[64px] bg-gray-200 rounded-xl p-1 opacity-0 hover:opacity-100 transition-opacity"
+                                onClick={onClearAmount}
+                            >
+                                ✕ 
+                            </button>
+                        )}
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                    
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency.toUpperCase()}
+                            </option>
+                        ))}
+                
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
